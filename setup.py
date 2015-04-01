f9de88c8973980a219c7c8a71162edcce5a160cf
Gregory Smith <gps@google.com>
2015-04-01 16:45:49 -0700

    Increase version number to 1.0.1.

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
+# Copyright 2015 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 """Simple distutils setup for the pure Python portpicker."""
 
+import distutils.core
 import sys
 import textwrap
-import distutils.core
 
 
 def main():
@@ -19,12 +33,12 @@ def main():
 
     distutils.core.setup(
         name='portpicker',
-        version='1.0.0',
+        version='1.0.1',
         description='A library to choose unique available network ports.',
-        long_description=textwrap.dedent("""
+        long_description=textwrap.dedent("""\
           Portpicker provides an API to find and return an available network
           port for an application to bind to.  Ideally suited for use from
-          unittests or for test harnesses that launch a local server."""),
+          unittests or for test harnesses that launch local servers."""),
         license='Apache 2.0',
         maintainer='Google',
         url='https://github.com/google/python_portpicker',
