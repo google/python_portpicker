391cbf2ecd7895a7cb11d2ee0e962aec051e71cc
Gregory P. Smith <gps@google.com>
2021-05-11 20:37:28 -0700

    Travis config update. (#22)
    * test on supported Python 3 versions.
    * switch away from test.sh for CI
    * drop 2.7.  it's EOL.

--- test.sh
+++ test.sh
@@ -1,25 +1,27 @@
 #!/bin/sh -ex
 
-echo 'TESTING under Python 3'
-mkdir -p build/test_envs/python3
-python3 -m venv build/test_envs/python3
-# Without --upgrade pip won't copy local changes over to a new test install
-# unless you've updated the package version number.
-build/test_envs/python3/bin/pip install --upgrade .
-build/test_envs/python3/bin/python3 src/tests/portpicker_test.py
+if which python3 >/dev/null ; then
+  echo 'TESTING under Python 3'
+  mkdir -p build/test_envs/python3
+  python3 -m venv build/test_envs/python3
+  # Without --upgrade pip won't copy local changes over to a new test install
+  # unless you've updated the package version number.
+  build/test_envs/python3/bin/pip install --upgrade pip
+  build/test_envs/python3/bin/pip install --upgrade .
+  build/test_envs/python3/bin/python3 src/tests/portpicker_test.py
 
-echo 'TESTING the portserver'
-PYTHONPATH=src build/test_envs/python3/bin/python3 src/tests/portserver_test.py
+  echo 'TESTING the portserver'
+  PYTHONPATH=src build/test_envs/python3/bin/python3 src/tests/portserver_test.py
+fi
 
 if which python2.7 >/dev/null ; then
-echo 'TESTING under Python 2.7'
-mkdir -p build/test_envs/python2
+  echo 'TESTING under Python 2.7'
+  mkdir -p build/test_envs/python2
   virtualenv --python=python2.7 build/test_envs/python2
   build/test_envs/python2/bin/pip install mock
+  build/test_envs/python2/bin/pip install --upgrade pip
   build/test_envs/python2/bin/pip install --upgrade .
   build/test_envs/python2/bin/python2 src/tests/portpicker_test.py
-else
-  echo 'Python 2.7 not found, not testing on that obsolete thing...'
 fi
 
 echo PASS
