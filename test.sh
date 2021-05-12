1153eb1c96670c73ab8af73f124f66f750612ebe
Gregory P. Smith [Google LLC] <gps@google.com>
2021-05-11 19:22:52 -0700

    Don't test on py2 when unavailable.

--- test.sh
+++ test.sh
@@ -1,21 +1,25 @@
 #!/bin/sh -ex
 
-echo 'TESTING under Python 2'
-mkdir -p build/test_envs/python2
-virtualenv --python=python2 build/test_envs/python2
-build/test_envs/python2/bin/pip install mock
-# Without --upgrade pip won't copy local changes over to a new test install
-# unless you've updated the package version number.
-build/test_envs/python2/bin/pip install --upgrade .
-build/test_envs/python2/bin/python2 src/tests/portpicker_test.py
-
 echo 'TESTING under Python 3'
 mkdir -p build/test_envs/python3
-virtualenv --python=python3 build/test_envs/python3
+python3 -m venv build/test_envs/python3
+# Without --upgrade pip won't copy local changes over to a new test install
+# unless you've updated the package version number.
 build/test_envs/python3/bin/pip install --upgrade .
 build/test_envs/python3/bin/python3 src/tests/portpicker_test.py
 
 echo 'TESTING the portserver'
 PYTHONPATH=src build/test_envs/python3/bin/python3 src/tests/portserver_test.py
+
+if which python2.7 >/dev/null ; then
+echo 'TESTING under Python 2.7'
+mkdir -p build/test_envs/python2
+  python2.7 -m venv build/test_envs/python2
+  build/test_envs/python2/bin/pip install mock
+  build/test_envs/python2/bin/pip install --upgrade .
+  build/test_envs/python2/bin/python2 src/tests/portpicker_test.py
+else
+  echo 'Python 2.7 not found, not testing on that obsolete thing...'
+fi
 
 echo PASS
