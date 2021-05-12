63a62940e1da30c36377e335ad83f8033fd313a5
Gregory P. Smith [Google LLC] <gps@google.com>
2021-05-11 22:23:24 -0700

    Use tox.

--- test.sh
+++ test.sh
@@ -1,27 +1,7 @@
 #!/bin/sh -ex
 
-if which python3 >/dev/null ; then
-  echo 'TESTING under Python 3'
-  mkdir -p build/test_envs/python3
-  python3 -m venv build/test_envs/python3
-  # Without --upgrade pip won't copy local changes over to a new test install
-  # unless you've updated the package version number.
-  build/test_envs/python3/bin/pip install --upgrade pip
-  build/test_envs/python3/bin/pip install --upgrade .
-  build/test_envs/python3/bin/python3 src/tests/portpicker_test.py
+python3 -m venv build/venv
+. build/venv/bin/activate
 
-  echo 'TESTING the portserver'
-  PYTHONPATH=src build/test_envs/python3/bin/python3 src/tests/portserver_test.py
-fi
-
-if which python2.7 >/dev/null ; then
-  echo 'TESTING under Python 2.7'
-  mkdir -p build/test_envs/python2
-  virtualenv --python=python2.7 build/test_envs/python2
-  build/test_envs/python2/bin/pip install mock
-  build/test_envs/python2/bin/pip install --upgrade pip
-  build/test_envs/python2/bin/pip install --upgrade .
-  build/test_envs/python2/bin/python2 src/tests/portpicker_test.py
-fi
-
-echo PASS
+pip install --upgrade pip tox twine build
+tox -e "py3$(python -c 'import sys; print(sys.version_info.minor)')"
