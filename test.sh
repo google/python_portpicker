e7ec68c5c04b65f062b10031dd24b13db96dbd4e
Gregory P. Smith [Google LLC] <gps@google.com>
2021-11-09 02:06:44 +0000

    1.5.0 Release.

--- test.sh
+++ test.sh
@@ -6,4 +6,7 @@ python3 -m venv build/venv
 
 pip install --upgrade pip
 pip install tox
+# We should really do this differently, test from a `pip install .` so that
+# testing relies on the setup.cfg install_requires instead of listing it here.
+pip install psutil
 tox -e "py3$(python -c 'import sys; print(sys.version_info.minor)')"
